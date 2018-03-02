f24701897a83051fc4ce06d97e9b983d56672721
subash karki <karki011@gmail.com>
2018-03-01 21:51:09 -0600

    Completed Step 1-4 Part 1

---
+++ AspNetCoreKata/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreKata.Models;
+
+namespace AspNetCoreKata.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list =  new List<Person>
+            {
+                new Person
+                {
+                    Name = "Subash Karki",
+                    Age = 25
+                },
+                new Person
+                {
+                    Name = "Subash Karki",
+                    Age = 25
+                },
+                new Person
+                {
+                    Name = "Subash Karki",
+                    Age = 25
+                }, new Person
+                {
+                    Name = "Subash Karki",
+                    Age = 25
+                },
+
+            };
+
+            return View(list);
+        }
+    }
+}
